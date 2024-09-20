25eafa899370dbb793854031d9104ed08103247e
WednesdaySP <shashiprabha2737@gmail.com>
2024-09-21 00:54:55 +0530

    leetcode rotation array done on 20 sept 2024

--- LeetCode/RotationCount.java
+++ LeetCode/RotationCount.java
@@ -1,46 +1,17 @@
 package LeetCode;
-// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1396704327/
-public class RotatedArray {
+
+public class RotationCount {
     public static void main(String[] args) {
-        int arr[]={4,5,6,7,9,2,3};
-        int target=5;
-       System.out.println(search(arr, target));
+        int [] arr={4,5,6,7,9,2,3};
+        System.out.println(countRotations(arr));
     }
-    static int search(int[] arr, int target){
+    public static int countRotations(int[] arr) {
         int pivot=findPivot(arr);
-        
-        //pivot not found
-        if(pivot==-1){
-            return binarySearch(arr, target, 0, arr.length-1);
-        }
-        //if pivot found, you have found two asc sorted array
-        if(arr[pivot]==target){
-            return pivot;
-        }
-        if(target >=arr[0]){
-            return binarySearch(arr, target, 0, pivot-1);
-        }
-        return binarySearch(arr, target, pivot+1, arr.length-1);
-    }
-
-    static int binarySearch(int[] arr, int target,int start, int end){
-        
-        
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]>target){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-        }
-        return -1;
+        return pivot+1;
 
     }
+
+    //use this for non duplicates
     static int findPivot(int[] arr){
         int start=0;
         int end=arr.length-1;
@@ -62,7 +33,7 @@ public class RotatedArray {
         return -1;
     }
 
-    static int findPivotwithDuplicates(int[] arr){
+    static int findPivotwithDuplicates(int[] arr,int target){
         int start=0;
         int end=arr.length-1;
         while(start<=end){
