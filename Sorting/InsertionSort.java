5f151f45fdcbb4c61da15ed64f9b0848f4746efe
WednesdaySP <shashiprabha2737@gmail.com>
2024-10-02 23:43:17 +0530

    1 oct 2024

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
     static void insertion(int[] arr){
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j>0;j--){
-                if(arr[j]<arr[j-1]){
+                if(arr[j]<arr[j-1]){ 
                     swap(arr, j, j-1);
                 }
                 else{
