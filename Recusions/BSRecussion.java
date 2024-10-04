4e2b579732f3802a460d75c33b9f583414c6a698
WednesdaySP <shashiprabha2737@gmail.com>
2024-10-04 23:34:19 +0530

    04 oct 2024 recursion

---
+++ Recusions/BSRecussion.java
@@ -0,0 +1,24 @@
+package Recusions;
+
+public class BSRecussion {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,95,65,34,65};
+        System.out.println(search(arr, 95, 0, arr.length-1));
+    }
+    static int search(int [] arr, int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int m=s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if(arr[m] > target){
+            return search(arr, target, s, m-1);
+        }
+        return search(arr, target, m+1, e);
+
+
+
+    }
+}
---
+++ Recusions/fiboRec.java
@@ -0,0 +1,21 @@
+package Recusions;
+
+public class fiboRec {
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(fibo(n));
+      
+
+        
+    }
+
+    static int fibo(int n){
+      
+     if(n<2){
+            return n;
+        }
+        
+            return fibo(n-1)+ fibo(n-2);
+        
+    }
+}
