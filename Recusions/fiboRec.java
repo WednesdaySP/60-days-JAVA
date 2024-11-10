cf4639a1da5ed0d9fbc809f44f30581ce4dec218
WednesdaySP <shashiprabha2737@gmail.com>
2024-11-10 12:51:55 +0530

    fibonacci using formula

--- Recusions/fiboRec.java
+++ Recusions/fiboRec.java
@@ -2,9 +2,18 @@ package Recusions;
 
 public class fiboRec {
     public static void main(String[] args) {
-        int n=10;
-        System.out.println(fibo(n));
+        // int n=10;
+        // System.out.println(fibo(n));
+
+        // for(int i=0;i<11;i++){
+        //     System.out.println(fiboFormula(i));
+        // }
+        System.out.println(fiboFormula(50));
       
+    }
+    static long fiboFormula(int n){
+        //return (int) ((Math.pow(((1+Math.sqrt(5))/2), n) - Math.pow(((1- Math.sqrt(5))/2), n))/Math.sqrt(5)); //original formula
+        return (long) (Math.pow(((1+Math.sqrt(5))/2), n)/Math.sqrt(5) );   ///formula after removing less domainating terms.
     }
 
     static int fibo(int n){
