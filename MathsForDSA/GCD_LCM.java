0c9bb449dc074cbebaf6557f39de9fe441eb6e67
WednesdaySP <shashiprabha2737@gmail.com>
2024-11-25 00:50:52 +0530

    24/11/24

---
+++ MathsForDSA/GCD_LCM.java
@@ -0,0 +1,15 @@
+package MathsForDSA;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(gcd(4, 8));
+        
+    }
+
+    static int gcd(int a, int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+}
